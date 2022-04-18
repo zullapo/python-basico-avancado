d50be90720f3cbd2268bcbda3acfaca41e8f8464
zullapo <pcluz62@gmail.com>
2022-04-18 17:28:44 -0300

    Aula 44 - Desafio Seção: Python Intermediário Descrição: Listas, Matrizes e Conjuntos

---
+++ python-intermediario/aula44/desafio.py
@@ -0,0 +1,51 @@
+"""
+Aula 44 - Desafio - Listas, Matrizes e Conjuntos
+
+-> É uma lista de listas de números inteiros
+-> As listas internas tem o tamanho de 10 elementos
+-> As listas internas contém números entre 1 a 10 e eles podem ser duplicados
+
+Exercício
+
+-> Crie uma função que encontra o primeiro duplicado considerando o segundo
+    número como a duplicação. Retorne a duplicação considerada.
+        Requisitos:
+            A ordem do número duplicado é considerada a partir da segunda
+            ocorrência do número, ou seja, o número duplicado em si.
+            Exemplo:
+                [1, 2, 3, ->3<-, 2, 1] -> 1, 2 e 3 são duplicados (retorne 3)
+                [1, 2, 3, 4, 5, 6] -> Retorne -1 (não tem duplicados)
+
+            Se não encontrar duplicados na lista, retorne -1
+"""
+
+matriz_numeros = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    [9, 1, 8, 9, 9, 7, 2, 1, 6, 8],
+    [1, 3, 2, 2, 8, 6, 5, 9, 6, 7],
+    [3, 8, 2, 8, 6, 7, 7, 3, 1, 9],
+    [4, 8, 8, 8, 5, 1, 10, 3, 1, 7],
+    [1, 3, 7, 2, 2, 1, 5, 1, 9, 9],
+    [10, 2, 2, 1, 3, 5, 10, 5, 10, 1],
+    [1, 6, 1, 5, 1, 1, 1, 4, 7, 3],
+    [1, 3, 7, 1, 10, 5, 9, 2, 5, 7],
+    [4, 7, 6, 5, 2, 9, 2, 1, 2, 1],
+    [5, 3, 1, 8, 5, 7, 1, 8, 8, 7],
+    [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+]
+
+
+def retorna_primeiro_duplicado(mt):
+    for i in range(len(mt)):
+        conjunto = set()
+        duplicado = -1
+        for j in range(len(mt[i])):
+            item = mt[i][j]
+            if item in conjunto:
+                duplicado = item
+                break
+            conjunto.add(item)
+        print(duplicado)
+
+
+retorna_primeiro_duplicado(matriz_numeros)
